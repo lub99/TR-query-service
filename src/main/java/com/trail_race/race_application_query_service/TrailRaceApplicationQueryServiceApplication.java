12219926d5330944a3e15a8db753eb65ea385822
lub99 <matej.luburic@gmail.com>
2024-11-17 23:09:32 +0100

    Initial commit

---
+++ src/main/java/com/trail_race/race_application_query_service/TrailRaceApplicationQueryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.trail_race.race_application_query_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrailRaceApplicationQueryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrailRaceApplicationQueryServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trail_race/race_application_query_service/TrailRaceApplicationQueryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trail_race.race_application_query_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrailRaceApplicationQueryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
